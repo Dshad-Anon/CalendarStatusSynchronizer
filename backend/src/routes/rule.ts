ed23377c553ab053c0965a0857f186d3e0bc1ad4
shiva <shiva@DSHAD>
2026-03-27 00:32:58 +0200

    added authentication in the rules.

--- backend/src/routes/rule.ts
+++ backend/src/routes/rule.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { createRule, deleteRule, getRules, updateRule } from "../controllers/ruleController";
+import { authenticate } from "../middleware/auth";
 
 const router = Router();
+router.use(authenticate);
+
 router.post("/", createRule);
 router.get("/", getRules);
 router.put("/:id", updateRule);
