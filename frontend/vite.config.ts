f24e3a59a2e6ec3483a2f80b50314c9e665c9f67
shiva <shiva@DSHAD>
2026-04-12 17:24:05 +0300

    added basic ssl for making https request which slack prevents.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,13 @@
 import react from "@vitejs/plugin-react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   server: {
+    https: {},
     port: 5173,
+    strictPort: true,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
